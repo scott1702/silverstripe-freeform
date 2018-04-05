c0ac764c7ebf7ea5a2388004cd2ff08eb34b2b44
Scott Hutchinson <scott@silverstripe.com>
2018-04-05 16:59:57 +1200

    Add jquery dependency

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 let mix = require('laravel-mix');
 
 mix.js('client/src/app.js', 'client/dist')
-   .sass('src/app.scss', 'client/dist')
+   // .sass('src/app.scss', 'client/dist')
    .setPublicPath('client/dist');
